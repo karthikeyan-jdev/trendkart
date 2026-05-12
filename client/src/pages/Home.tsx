337f00436f8bfcef3d824e9b8ae43e238295d6ff
karthikeyan.jdev <karthikeyan.jdev@gmail.com>
2026-05-12 09:23:21 +0530

    test

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,16 +2,53 @@ import { useEffect, useState } from "react";
 
 const Home = () => {
   const [products, setProducts] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
   const API_URL = import.meta.env.VITE_API_URL;
+
   useEffect(() => {
     const getProduct = async () => {
-      const res = await fetch(`${API_URL}/api/products`);
-      const data = await res.json();
-      setProducts(data);
+      try {
+        setLoading(true);
+        setError("");
+
+        const res = await fetch(`${API_URL}/api/products`);
+
+        if (!res.ok) {
+          throw new Error("Failed to fetch products");
+        }
+
+        const data = await res.json();
+
+        setProducts(data);
+      } catch (err: any) {
+        setError(err.message || "Something went wrong");
+      } finally {
+        setLoading(false);
+      }
     };
 
     getProduct();
   }, []);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-2xl font-semibold text-blue-600 animate-pulse">
+          Loading...
+        </h1>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-2xl font-semibold text-red-600">{error}</h1>
+      </div>
+    );
+  }
 
   return (
     <div className="min-h-screen bg-gray-100 p-6">
@@ -22,18 +59,18 @@ const Home = () => {
         {products.map((item) => (
           <div
             key={item.id}
-            className="bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition "
+            className="bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition h-105 lg:h-110"
           >
             {/* Image */}
             <img
               src={item.images?.[0]}
-              alt={item.title}
+              alt={item.title || "Product Image"}
               className="w-fit h-[60%] mx-auto object-contain"
             />
 
             {/* Content */}
             <div className="p-4">
-              <h2 className="text-lg font-semibold line-clamp-2">
+              <h2 className="text-lg font-semibold line-clamp-1">
                 {item.title}
               </h2>
 
